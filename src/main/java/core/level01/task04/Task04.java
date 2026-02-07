0d09f4cf96c28a3505a8c6d108cce826f5c29754
Admin <dir.gefest@gmail.com>
2026-02-07 16:23:58 +0300

    Task04

---
+++ src/main/java/core/level01/task04/Task04.java
@@ -0,0 +1,18 @@
+package main.java.core.level01.task04;
+
+/**
+ * Объявите переменную типа int с именем currentYear. Присвойте ей значение 2025 и выведите это значение на экран.
+ *
+ * Требования:
+ * •	В программе должна быть объявлена переменная типа int с именем currentYear.
+ * •	Переменной currentYear должно быть присвоено значение 2025.
+ * •	Программа должна вывести на экран значение переменной currentYear.
+ */
+public class Task04 {
+    public static void main(String[] args) {
+        int currentYear = 2025;
+        System.out.println(currentYear);
+    }
+
+
+}

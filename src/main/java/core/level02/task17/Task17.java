59905427bc9dfcbedea489b61e2b7f7feb0fefce
Admin <dir.gefest@gmail.com>
2026-02-20 08:51:34 +0300

    Task 17

---
+++ src/main/java/core/level02/task17/Task17.java
@@ -0,0 +1,17 @@
+package main.java.core.level02.task17;
+
+/**
+ *Представьте, что вы получаете сообщение из будущего.
+ * Объявите переменную типа String с именем futureMessage. Присвойте ей значение "Good news everyone!" и выведите это сообщение на экран.
+ * Требования:
+ * •	В программе должна быть объявлена переменная с именем futureMessage типа String.
+ * •	Переменной futureMessage должно быть присвоено значение "Good news everyone!".
+ * •	Программа должна вывести на экран содержимое переменной futureMessage.
+ * •	Для вывода строки на экран необходимо использовать команду System.out.println.
+ */
+public class Task17 {
+    public static void main(String[] args) {
+        String futureMessage = "Good news everyone!";
+        System.out.println(futureMessage);
+    }
+}

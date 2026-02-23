352d8d5fbc484f1bb834c7ffcf205fd94f6f1294
Admin <dir.gefest@gmail.com>
2026-02-23 09:22:35 +0300

    Example1,2

---
+++ src/main/java/core/level03/Example1.java
@@ -0,0 +1,33 @@
+package main.java.core.level03;
+
+import java.util.Scanner;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите ваше имя: ");
+        String name = console.nextLine();
+
+        System.out.print("Введите ваш возраст: ");
+        int age = console.nextInt();
+
+        if (age >= 21)
+        {
+            System.out.print("Введите пригласительный код: ");
+            String code = console.nextLine();
+
+            if (code.equals("VIP2024"))
+            {
+                System.out.println("Добро пожаловать, " + name + ", в VIP-клуб!");
+            }
+            else
+            {
+                System.out.println("Извините, неверный код. Доступ запрещён.");
+            }
+        }
+        else
+        {
+            System.out.println("Извините, вход только с 21 года!");
+        }
+    }
+}
---
+++ src/main/java/core/level03/Example2.java
@@ -0,0 +1,32 @@
+package main.java.core.level03;
+
+import java.util.Scanner;
+
+public class Example2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        System.out.print("Введите ваш возраст: ");
+        int age = console.nextInt();
+        console.nextLine();
+        double discount = 0.0;
+
+        if (age >= 60)
+        {
+            discount = 0.15;
+        }
+        else
+        {
+            System.out.print("Есть ли у вас клубная карта (да/нет)? ");
+            String hasCard = console.nextLine();
+
+            if (hasCard.equals("да"))
+            {
+                discount = 0.07;
+            }
+        }
+
+        System.out.println("Ваша скидка: " + (discount * 100) + "%");
+
+    }
+}

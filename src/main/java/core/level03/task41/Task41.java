96133c0a8989f4e7ee885ea7ca04be93cd44bab0
Admin <dir.gefest@gmail.com>
2026-02-24 11:36:30 +0300

    Task41

---
+++ src/main/java/core/level03/task41/Task41.java
@@ -0,0 +1,18 @@
+package main.java.core.level03.task41;
+
+/**
+ * Вы пишете программу, чтобы проверить правдивость утверждения. Создайте переменную типа boolean с именем isJavaFun и присвойте ей значение true.
+ * Выведите это значение на экран.
+ * Требования:
+ * •	В программе должна быть объявлена переменная типа boolean с именем isJavaFun.
+ * •	Переменной isJavaFun должно быть присвоено значение true.
+ * •	Программа должна вывести на экран значение переменной isJavaFun.
+ * •	В коде не должно быть попыток использовать другие типы данных для переменной isJavaFun.
+ */
+public class Task41 {
+    public static void main(String[] args) {
+    boolean isJavaFun = true;
+        System.out.println(isJavaFun);
+
+    }
+}
